c6f5e224582aa2be8a764e0c8ee7f77f0779969d
PHIDELIST <phidelisoluoch@gmail.com>
2024-02-08 16:16:28 +0300

    configured azure access

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 provider "azurerm" {
   features {}
+   
 }
+data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "azure-test-keyvault" {
   name     = "keyvault-resources"
@@ -8,10 +10,11 @@ resource "azurerm_resource_group" "azure-test-keyvault" {
 }
 
 resource "azurerm_key_vault" "test" {
-  name                        = "test-keyvault"
+  name                        = "test-keyvault-testPoc"
   location                    = azurerm_resource_group.azure-test-keyvault.location
   resource_group_name         = azurerm_resource_group.azure-test-keyvault.name
   enabled_for_disk_encryption = true
+  tenant_id       = data.azurerm_client_config.current.tenant_id
 
   sku_name = "standard"
 
@@ -20,6 +23,20 @@ resource "azurerm_key_vault" "test" {
   }
 }
 
+resource "azurerm_key_vault_access_policy" "test_policy" {
+  key_vault_id = azurerm_key_vault.test.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
+
+  secret_permissions = [
+    "Get",       # Allow reading secrets
+    "List",      # Allow listing secrets
+    "Set",       # Allow setting (creating/updating) secrets
+    "Delete",    # Allow deleting secrets
+    
+  ]
+}
 resource "azurerm_key_vault_secret" "mysecrets" {
   name         = "person1"
   value        = "passwordstrong"
